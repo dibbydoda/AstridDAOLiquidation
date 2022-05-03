1f53deb67d233a64706bc213269b9fb4783cde4c
dibbydoda <dibbydoda@gmail.com>
2022-05-03 22:08:53 +1000

    try a from-to filter

--- AstridDAOBot.py
+++ AstridDAOBot.py
@@ -144,7 +144,7 @@ def main():
     sorted_vaults = w3connection.eth.contract(address=SORTED_VAULTS_ADDRESS, abi=SortedVaultsABI)
     price_feed = w3connection.eth.contract(address=PRICE_FEED_ADDRESS, abi=PriceFeedABI)
     oracle_feed = w3connection.eth.contract(address=DIA_ORACLE_ADDRESS, abi=DaiOracleABI)
-    block_filter = w3connection.eth.filter("pending")
+    block_filter = w3connection.eth.filter({'fromBlock': 'latest', 'toBlock': 'pending'})
 
     while True:
         for transaction in block_filter.get_new_entries():
